49ef212b00c0078dba327eff0c0a35bd89811c51
Miguel Ramirez O <mikivan@hotmail.com>
2021-12-07 00:49:51 -0600

    tabla dinamica

--- ActividadesComplementarias/app/src/main/java/com/example/actividadescomplementarias/AdminView.java
+++ ActividadesComplementarias/app/src/main/java/com/example/actividadescomplementarias/AdminView.java
@@ -2,13 +2,65 @@ package com.example.actividadescomplementarias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+
+import java.util.ArrayList;
 
 public class AdminView extends AppCompatActivity {
+    private TableLayout tableLayout;
+    private EditText txtName;
+    private EditText txtLastName;
+    private String[] header={"Id","Nombre","Apellido"};
+    private ArrayList<String[]> rows = new ArrayList<>();
+    private TableDynamic tableDynamic;
+    private Button btnEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_view);
+        setContentView(R.layout.activity_main);
+        tableLayout=(TableLayout) findViewById(R.id.table);
+        txtName=(EditText) findViewById(R.id.txtName);
+        txtLastName=(EditText) findViewById(R.id.txtLastName);
+        btnEditar=(Button)findViewById(R.id.btnEditarAct);
+
+        btnEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                save();
+            }
+        });
+
+        tableDynamic=new TableDynamic(tableLayout,getApplicationContext());
+        tableDynamic.addHeader(header);
+        tableDynamic.addData(getClients());
+        tableDynamic.backgroundHeader(Color.BLUE);
+        tableDynamic.backgroundData(Color.RED,Color.YELLOW);
+        tableDynamic.lineColor(Color.BLACK);
+        tableDynamic.textColorData(Color.WHITE);
+        tableDynamic.textColorHeader(Color.MAGENTA);
+
+    }
+
+
+
+    public void save(){
+        String[] item=new String[]{"5",txtName.getText().toString(),txtLastName.getText().toString()};
+        tableDynamic.addItems(item);
+    }
+
+    private ArrayList<String[]> getClients(){
+        rows.add(new String[]{"1","Pedro","Lopez"});
+        rows.add(new String[]{"2","Sofia","Alfaro"});
+        rows.add(new String[]{"3","Naomi","Espejel"});
+        rows.add(new String[]{"4","Lorena","Gallegos"});
+        return rows;
+
     }
 }
--- ActividadesComplementarias/app/src/main/java/com/example/actividadescomplementarias/MainActivity.java
+++ ActividadesComplementarias/app/src/main/java/com/example/actividadescomplementarias/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         loginBtn = (Button) findViewById(R.id.loginBtn);
 
         loginBtn.setOnClickListener((view)->{
-            Intent intent = new Intent(this, EditActividad.class);
+            Intent intent = new Intent(this, AdminView.class);
             startActivity(intent);
         });
     }
